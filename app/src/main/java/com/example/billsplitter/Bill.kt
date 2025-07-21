c65515313bd1f088af049b86a0e266e80688f292
Angelina Wang <angelinaw@berkeley.edu>
2025-07-20 18:28:52 -0700

    changed data class

--- app/src/main/java/com/example/billsplitter/Bill.kt
+++ app/src/main/java/com/example/billsplitter/Bill.kt
@@ -1,6 +1,6 @@
 package com.example.billsplitter
 
-data class Bill(val name: String, val subtotal: Double, val tax: Double, val total: Double, var dishes: List<Dish>) {
+data class Bill(val name: String, val subtotal: Double, val tax: Double, val total: Double, var dishes:  Map<Int, String> = mapOf()) {
     fun getTipPercentage(): Double {
         return (total - subtotal - tax) / subtotal
     }
